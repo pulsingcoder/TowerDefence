3397128a4004de9a941381fc7879463936d1a4f8
Yuvraj Darwani <44190710+pulsingcoder@users.noreply.github.com>
2020-06-26 10:35:16 +0530

    PathFinding

---
+++ DefenceTower/Assets/EnemyMovementScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementScript : MonoBehaviour
+{
+    [SerializeField] List<Block> Path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintAllWaypoints();
+
+    }
+
+    private void PrintAllWaypoints()
+    {
+        foreach (Block ways in Path)
+        {
+            print(ways.name + " ");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
